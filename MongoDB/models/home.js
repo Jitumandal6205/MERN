2c3ed4c447c2c22ee7aaeb45534f34a7e2a69dea
Jitumandal6205 <jitukr.169@gmail.com>
2025-05-13 00:31:29 +0530

    adding new codes

---
+++ MongoDB/models/home.js
@@ -0,0 +1,31 @@
+// Core Modules
+const db = require("../utils/databaseUtil");
+const {getDB} = require("../utils/databaseUtil");
+module.exports = class Home {
+  constructor(houseName, price, location, rating, photoUrl, description, id) {
+    this.houseName = houseName;
+    this.price = price;
+    this.location = location;
+    this.rating = rating;
+    this.photoUrl = photoUrl;
+    this.description = description;
+    this.id = id;
+  }
+
+  save() {
+    const db = db.getDB();
+    return db.collection('homes').insertOne(this);
+  }
+
+  static fetchAll() {
+   
+  }
+
+  static findById(homeId) {
+    
+  }
+
+  static deleteById(homeId) {
+    
+}
+};
